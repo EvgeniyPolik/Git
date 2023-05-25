11d516c7007124f689f67df2ff7615bf80459e81
PolikarpovE <poliex86@inbox.lv>
2023-05-25 14:07:38 +0500

    libmodbus prepare

---
+++ Simargl/Simargl.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <conio.h>
+#include <ctype.h>
+extern "C" {
+#include <modbus.h> 
+}
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
